ff525932fc8d2500f9d3e4adf895c46501318bf2
Sahana K V56 <sahakv56@in.ibm.com>
2021-10-29 20:32:49 +0530

    FE changes

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import LandingPage from './main/LandingPage';
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <LandingPage />
   </React.StrictMode>,
   document.getElementById('root')
 );
